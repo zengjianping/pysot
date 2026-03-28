d9725c1ff1490361b693d793a99c4a7eb5d7913d
zengjianping <happysjfzjp@126.com>
2026-03-28 20:09:25 +0800

    dev

--- scripts/export.sh
+++ scripts/export.sh
@@ -13,6 +13,6 @@ model_name="siamrpn_alex_dwxcorr"
 python tools/export.py \
     --config ${model_name} \
     --snapshot datas/models/${model_name}/model.pth \
-    --output datas/models/${model_name}/model_scripted.pt
+    --output datas/models/${model_name}/model.pt
 
 
